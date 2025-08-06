b0bfe517fb222d11cd79c3b79b2d2b8784cdb6a7
jrferguson <1q2w3e4r5t6y>
2025-08-06 19:57:00 +0100

    addded trainer section

--- src/app/components/cards/member-card/member-card.component.ts
+++ src/app/components/cards/member-card/member-card.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import type { MemberType } from '@/types';
+import { Component, Input } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import type { MemberType } from "@/types";
 
 @Component({
-    selector: 'app-member-card',
-    imports: [RouterLink],
-    templateUrl: './member-card.component.html',
-    styles: ``
+	selector: "app-member-card",
+	imports: [RouterLink],
+	templateUrl: "./member-card.component.html",
+	styles: ``,
 })
 export class MemberCardComponent {
-    @Input() member!: MemberType 
+	@Input() member: any;
+	getProfileImage(img: String) {
+		return img ? img : "assets/img/all-images/default-profile.png";
+	}
 }
