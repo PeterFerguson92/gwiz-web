f693cdf6d8052e12b9beee7e8e791a1f6439c4d0
jrferguson <1q2w3e4r5t6y>
2025-08-07 21:38:02 +0100

    fixed desktop topbar

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -23,13 +23,6 @@ export class TopbarComponent implements OnInit {
 	@Input() logo!: string;
 	@Input() isAlert?: boolean;
 
-	homeMenuItems = [
-		{ title: "Home 01", image: "assets/img/demo/demo-img1.png", multiPageLink: "/home-1", onePageLink: "/home-single-1" },
-		{ title: "Home 02", image: "assets/img/demo/demo-img2.png", multiPageLink: "/home-2", onePageLink: "/home-single-2" },
-		{ title: "Home 03", image: "assets/img/demo/demo-img3.png", multiPageLink: "/home-3", onePageLink: "/home-single-3" },
-		{ title: "Home 04", image: "assets/img/demo/demo-img4.png", multiPageLink: "/home-4", onePageLink: "/home-single-4" },
-	];
-
 	serviceMenuItems = [
 		{ title: "Service One", link: "/services/one" },
 		{ title: "Service Left", link: "/services/left" },
