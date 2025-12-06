24189a98a3b93a64c6886b57a66ca1fed07919e1
jrferguson <1q2w3e4r5t6y>
2025-12-06 03:00:31 +0000

    fixed qa env config

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiUrl: 'https://gwiz-admin-qa-a9f0452d617e.herokuapp.com/api',
   stripePublishableKey:
     'pk_test_51Sb9dMLT8MvjHZzurkwyjGMMhGnICvbh4tWhoyOkt7Qu7kBl7EG5MRN0DIqvdzxxpN8KV6qhVA27OicJ4Z9Ue53D00JlBO1wds',
 };
